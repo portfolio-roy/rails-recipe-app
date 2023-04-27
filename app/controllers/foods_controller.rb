062dd53b7d21c3e7851f5e8229d1cb2f76ddf956
Utsargo Roy <royutsargo@gmail.com>
2023-04-27 23:13:28 +0600

    Edit food controller to use authentication and API endpoints

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,31 +1,61 @@
 class FoodsController < ApplicationController
-  # load_and_authorize_resource
-  # before_action :authenticate_user!, only: [:create]
-  # before_action :set_food, only: %i[show edit update destroy]
+  before_action :authenticate_user!
+  before_action :set_food, only: %i[show edit update destroy]
+
+  # GET /foods or /foods.json
   def index
     @foods = Food.all
   end
 
+  # GET /foods/1 or /foods/1.json
+  def show; end
+
+  # GET /foods/new
   def new
     @food = Food.new
   end
 
+  # GET /foods/1/edit
+  def edit; end
+
+  # POST /foods or /foods.json
   def create
-    @food = Food.new(
-      name: params[:food][:name],
-      measurement_unit: params[:food][:measurement_unit],
-      quantity: params[:food][:quantity],
-      price: params[:food][:price],
-      user_id: 1
-    )
-    @food.save
-    redirect_to foods_path
+    @food = Food.new(food_params)
+    @food.user = current_user
+
+    respond_to do |format|
+      if @food.save
+        format.html { redirect_to foods_url, notice: 'Food was successfully created.' }
+        format.json { render :index, status: :created, location: @food }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @food.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # PATCH/PUT /foods/1 or /foods/1.json
+  def update; end
+
+  # DELETE /foods/1 or /foods/1.json
   def destroy
-    @food = Food.find(params[:id])
-    # authorize! :destroy, @food
     @food.destroy
-    redirect_to foods_path, notice: 'Food was successfully deleted.'
+
+    respond_to do |format|
+      format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_food
+    @food = Food.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
 end
