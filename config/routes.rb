3e5757f4818a134144a5ef8541b27ba295008dcb
Utsargo Roy <royutsargo@gmail.com>
2023-04-28 01:00:01 +0600

    Add show path for foods and create a template for this

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root "foods#index"
   devise_for :users
-root 'foods#index'
-resources :recipes
-resources :foods, only: [:index, :new, :create, :destroy]
+  resources :recipes
+  resources :foods, only: [:index, :show, :new, :create, :destroy]
 end
