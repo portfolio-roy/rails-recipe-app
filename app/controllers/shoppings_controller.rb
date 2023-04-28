beaa40685ed71819c918df5c57c7e09dc34f2997
Juan Diaz <jfdiaza@unal.edu.co>
2023-04-28 10:36:47 -0400

    fix linters issues

--- app/controllers/shoppings_controller.rb
+++ app/controllers/shoppings_controller.rb
@@ -1,9 +1,6 @@
 class ShoppingsController < ApplicationController
-  
-    def show
-        @recipe = Recipe.includes(:recipe_food).find(params[:recipe_id])
-        @ingredients = @recipe.recipe_food.where(recipe: @recipe)
-    end
-  
+  def show
+    @recipe = Recipe.includes(:recipe_food).find(params[:recipe_id])
+    @ingredients = @recipe.recipe_food.where(recipe: @recipe)
   end
-  
+end
