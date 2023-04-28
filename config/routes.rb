18d1d54b18f86889fc8f9ee840722048d0343cc3
Utsargo Roy <royutsargo@gmail.com>
2023-04-29 01:54:44 +0600

    Add custom homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  root "foods#index"
+  root "home#index"
   devise_for :users
   resources :recipes do
     resources :recipe_foods, only: [:new, :create, :destroy, :show, :edit, :update]
     resources :shoppings, only: [:show]
     get "shoppings", to: "shoppings#index"
-end
+  end
   resources :foods, only: [:index, :show, :new, :create, :destroy]
   get "public_recipes", to: "public_recipes#index"
+  get 'not_accessible', to: 'errors#not_accessible'
+ 
 end
