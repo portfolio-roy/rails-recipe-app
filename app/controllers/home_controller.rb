71dbf01130b514d90962bff2973050ce745a6c1b
Juan Diaz <jfdiaza@unal.edu.co>
2023-04-29 01:02:58 -0400

    style views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-    def index
-        @public_recipes = Recipe.includes(:recipe_food, :user).where(public: true).order(created_at: :desc)
-      end
+  def index
+    @public_recipes = Recipe.includes(:recipe_food, :user).where(public: true).order(created_at: :desc)
+  end
 end
